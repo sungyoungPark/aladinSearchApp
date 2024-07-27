1ed89a138517bbc932225da85a2f1d9ae3d0b395
sungyoungPark <sygitter1491012@gmail.com>
2024-07-28 01:35:50 +0900

    Feat : Product 페이지 구성

---
+++ aladinSearchApp/Reactor/ProductReactor.swift
@@ -0,0 +1,29 @@
+//
+//  ProductReactor.swift
+//  aladinSearchApp
+//
+//  Created by 박성영 on 7/28/24.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+import ReactorKit
+
+class ProductReactor : Reactor {
+    
+    let initialState = State()
+    
+    enum Action {
+       
+    }
+    
+    enum Mutation {
+       
+    }
+    
+    struct State {
+       
+    }
+    
+}
--- aladinSearchApp/View/ProductViewController.swift
+++ aladinSearchApp/View/ProductViewController.swift
@@ -6,15 +6,32 @@
 //
 
 import UIKit
+import SnapKit
+import ReactorKit
+import RxSwift
+import RxCocoa
 
-class ProductViewController: UIViewController {
+class ProductViewController: UIViewController, View {
 
+    typealias Reactor = ProductReactor
+    
+    var disposeBag: DisposeBag = DisposeBag()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        setupUI()
     
     }
     
+    func setupUI() {
+       
+    }
+    
 
+    func bind(reactor: ProductReactor) {
+        
+    }
 
 }
